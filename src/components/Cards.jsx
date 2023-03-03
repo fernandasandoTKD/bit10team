2f7121b3a2835f68eee2ca9943866a834c7c3ac7
Luisa Fer <l.f.d.sando@gmail.com>
2023-03-02 23:23:29 -0500

    modificación nosotros

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,23 +1,50 @@
-import React from 'react'
+import React, {useState} from 'react'
 import CardAbout from './CardAbout'
 import './css/card.css';
+import mari from '../img/1.png';
+import eus from '../img/2.png';
+import lu from '../img/3.png';
 
-function Cards() {
-  return (
-    <div className="container ">
-        <div className="row ">
-            <div className="col-md-4">
-                <CardAbout></CardAbout>
-            </div>
-            <div className="col-md-4">
-                <CardAbout></CardAbout>
-            </div>
-            <div className="col-md-4">
-                <CardAbout></CardAbout>
-            </div>
-        </div>
-    </div>
-  )
-}
+const Cards = () => {
+    const [persons, setPersons] = useState([
+      {
+        id: 1,
+        role: "Lic. en Administración y aprendiz de desarrollo web Frontend con enfasis en React JS. Continuamente aprendiendo y adquiriendo nuevas habilidades en función a las actuales y futuras demandas y tendencias tecnológicas en el campo del diseño y desarrollo web en general.",
+        img: mari,
+        url: "https://maribsu1.github.io/bit07me/"
+      },
+      {
+        id: 2,
+        role: "Profesional desempeñada en el área educativa con experiencia en la enseñanza de lenguajes de programación en las ramas de Frontend y Backend, procesos de ejecución de ciclos y reportes deincidencia, desde la planificación y diseño de interfaz de usuario.",
+        img: lu ,
+        url: "https://fernandasandotkd.github.io/bit07me/"
+      },
+      {
+        id: 3,
+        role: "Aprendiz desarrollo web Front-End Jr con conocimientos en lenguajes de maquetado y demarcado, y programación en JavaScript con uso de framework React, en continuo aprendizaje de forma didáctica, dispuesta siempre a nuevos retos",
+        img: eus,
+        url: "https://eusbeidy.github.io/bit07me/",
+      },
+    ]);
+  
+    return (
+    <div className="container">
 
+    
+      <div className="row">
+        {persons.map((person) => {
+          return (
+            <CardAbout
+              key={person.id}
+              img={person.img}
+              role={person.role}
+              url={person.url}
+            />
+          );
+        })}
+      </div>
+      </div>
+    );
+  };
+  
 export default Cards
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ function Footer() {
     
     <>
     <div >
-        <footer className="py-3 my-4 bg-dark" >
+        <footer className="py-2 my-2 bg-dark" >
             <ul className="nav justify-content-center border-bottom pb-3 mb-3">
                 <li className="nav-item"><a href="https://www.facebook.com/BogotaInstituteOfTechnology" className="nav-link px-2 text-muted" target={'_blank'}><img src={facebook} alt="" /></a></li>
                 <li className="nav-item"><a href="https://www.youtube.com/@bit_institute" className="nav-link px-2 text-muted" target={'_blank'}><img src={youtube} alt="" /></a></li>
